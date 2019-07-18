f8e1a2d22d79ae12f5d7197d6d4f399df0bbf2a5
Alec Laws <alec@identos.ca>
2019-07-17 21:47:32 -0400

    Fetch and view immunization data

---
+++ src/services/FHIRResource.js
@@ -0,0 +1,41 @@
+
+import FHIRUserAuthz from './FHIRUserAuthz'
+
+
+
+import FHIRApi from './api/FHIRAPI'
+
+
+const FHIRResource = {
+
+	getImmunizations
+}
+
+
+/**
+	medications, immunization, lab_results
+*/
+function getImmunizations() {
+
+	const access = FHIRUserAuthz.getGrantedPermission("immunization")
+	if(!access) { return Promise.reject("no access") }
+
+
+	return new Promise(function(resolve, reject) {
+
+		FHIRApi.resourceRequest(access.resource_location, access.token)
+		.then(result => {
+			if(result.status !== 200) { reject("bad authz") }
+
+	        const resources = result.data.entry.map(x => x.resource)
+
+            const immz = resources.filter(x => x.resourceType === "Immunization")
+            const patient = resources.filter(x => x.resourceType === "Patient")[0]
+
+			resolve({immunizations: immz, patient: patient})
+		},
+		(error) => reject(error))
+	})
+}
+
+export default FHIRResource
